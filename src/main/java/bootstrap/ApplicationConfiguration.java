660ac3f080cb0ed3403ae68c2583b83880359f53
James Earl Douglas <james@earldouglas.com>
2012-08-04 12:32:58 -0700

    initial commit

---
+++ src/main/java/bootstrap/ApplicationConfiguration.java
@@ -0,0 +1,40 @@
+package bootstrap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import bootstrap.service.CommentsService;
+import bootstrap.service.InMemCommentsService;
+import bootstrap.web.CommentsController;
+import bootstrap.web.IndexController;
+
+@EnableWebMvc
+@Configuration
+public class ApplicationConfiguration {
+
+	@Bean
+	public CommentsService commentsService() {
+		return new InMemCommentsService();
+	}
+
+	@Bean
+	public IndexController indexController() {
+		return new IndexController();
+	}
+
+	@Bean
+	public CommentsController commentsController() {
+		return new CommentsController();
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver jspViewResolver = new InternalResourceViewResolver();
+		jspViewResolver.setPrefix("/WEB-INF/views/");
+		jspViewResolver.setSuffix(".jsp");
+		return jspViewResolver;
+	}
+}
